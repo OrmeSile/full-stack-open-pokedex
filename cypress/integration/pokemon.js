7fbf07a4e1fbadbd33f5a08dd47b2a50854bb50c
OrmeSile <95030000+OrmeSile@users.noreply.github.com>
2022-05-28 18:02:26 +0200

    done 11

--- cypress/integration/pokemon.js
+++ cypress/integration/pokemon.js
@@ -1,7 +1,16 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+/* eslint-disable indent */
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('single pokemon page can be navigated to from front page'),
+    function () {
+      cy.visit('http://localhost:5000')
+      cy.contains('bulbasaur').click()
+      cy.contains('Chlorophyll')
+      cy.contains('Overgrow')
+    }
 })
